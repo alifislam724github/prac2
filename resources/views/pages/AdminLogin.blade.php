41fbe0672b0fd7e9aa3fb0d9e650a9efe87eab79
alifislam724github <alifislam724@gmail.com>
2022-03-12 20:40:09 +0600

    bootstrap

--- resources/views/pages/AdminLogin.blade.php
+++ resources/views/pages/AdminLogin.blade.php
@@ -1,22 +1,35 @@
 @extends('layout.app')
 @section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+</head>
+<body style="background-image: url('images/login11.jpg')">
+<div class=" col-lg-6 ">
+    <br>
+<div style="background-image: url('images/registration.jpg')" >
 <form action="{{route('AdminLogin')}}" class="form-group" method="post">
 {{csrf_field()}}
-<label for="">Email</label>
-    <input type="text" class="form-control" name="email" value="{{old('email')}}">
+<label class="text-uppercase fw-bold" for="">Email</label>
+    <input type="text" class="form-control bg-light" name="email" value="{{old('email')}}">
     @error('email')
     <span>{{$message}}</span>
     @enderror
     <br>
-    <label for="">Password</label>
-    <input type="password" class="form-control" name="password" value="{{old('password')}}">
+    <label class="text-uppercase fw-bold" for="">Password</label>
+    <input type="password" class="form-control bg-light" name="password" value="{{old('password')}}">
     @error('password')
     <span>{{$message}}</span>
     @enderror
     <br>
     
-    <input type="submit">
+    <button type="button" class= "btn btn-light">Log In</button>
 </form>
+
 
 </body>
 </html>
--- resources/views/pages/AdminRegistration.blade.php
+++ resources/views/pages/AdminRegistration.blade.php
@@ -1,42 +1,60 @@
 @extends('layout.app')
 @section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Registration</title>
+</head>
+<body style="background-image: url('images/registration1.jpg')">
+<div class=" col-lg-6 ">
+    
+<div style="background-image: url('images/registration.jpg')" >
+
 <form action="{{route('AdminRegistration')}}" class="form-group" method="post">
 {{csrf_field()}}
-    <label for="">Admin Name</label>
-    <input type="text" class="form-control" name="name" value="{{old('name')}}">
+    
+    <br>
+    <label class="text-uppercase fw-bold" for="">Admin Name</label>
+    <input type="text" class="form-control bg-light" name="name" value="{{old('name')}}">
     @error('name')
     <span>{{$message}}</span>
     @enderror
     <br>
-    <label for="">Password</label>
-    <input type="password" class="form-control" name="password" value="{{old('password')}}">
+    <label class="text-uppercase fw-bold" for="">Password</label>
+    <input type="password" class="form-control bg-light " name="password" value="{{old('password')}}">
     @error('password')
     <span>{{$message}}</span>
     @enderror
     <br>
-    <label for="">Email</label>
-    <input type="text" class="form-control" name="email" value="{{old('email')}}">
+    <label class="text-uppercase fw-bold" for="">Email</label>
+    <input type="text" class="form-control bg-light" name="email" value="{{old('email')}}">
     @error('email')
     <span>{{$message}}</span>
     @enderror
     <br>
-    <label for="">Phone no</label>
-    <input type="text" class="form-control" name="phoneno" value="{{old('phoneno')}}">
+    <label class="text-uppercase fw-bold" for="">Phone no</label>
+    <input type="text" class="form-control bg-light" name="phoneno" value="{{old('phoneno')}}">
     @error('phoneno')
     <span>{{$message}}</span>
     @enderror
     <br>
-    <label for="">Address</label>
-    <input type="text" class="form-control" name="address" value="{{old('address')}}">
+    <label class="text-uppercase fw-bold" for="">Address</label>
+    <input type="text" class="form-control bg-light" name="address" value="{{old('address')}}">
     @error('address')
     <span>{{$message}}</span>
     @enderror
     <br>
    
 
-    <input type="submit">
+    <button type="button" class= "btn btn-light">Submit</button>
 </form>
-
+</div>
+</div>
 </body>
 </html>
 @endsection
+
+
